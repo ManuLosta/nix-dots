4dd2370f92423beae4c659798c8589cdd5fe6616
ManuLosta <mlostalom@gmail.com>
2024-07-07 00:43:19 -0300

    add gaming

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -91,6 +91,7 @@
     neovim
     wget
     killall
+		mangohud
   ];
 
   # Enable Hyprland
@@ -128,6 +129,22 @@
   # Enable ZSH
   programs.zsh.enable = true;
   users.users.manuel.shell = pkgs.zsh;
+
+	# Enable Steam
+	programs.steam = {
+		enable = true;
+		remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+		dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+		localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+		gamescopeSession.enable = true;
+	};
+
+	programs.gamemode.enable = true;
+
+	hardware.graphics = {
+		enable = true;
+		enable32Bit = true;
+	};
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
